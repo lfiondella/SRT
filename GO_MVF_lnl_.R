49d413d79b70e2497f4534e72e537a6c1eace5f0
Robert Davis <rdavis2@umassd.edu>
2015-04-14 15:51:46 -0400

    simplified code

--- GO_MVF_lnl_.R
+++ GO_MVF_lnl_.R
@@ -1,30 +1,15 @@
 #Mean Value Function
-GO_BM_FT_MVF <- function(aMLE,bMLE,data){
-		tmp <- aMLE*(1-exp(-bMLE*data))
-		return(tmp)
-}
+GO_BM_FT_MVF <- function(aMLE,bMLE,data){		return(aMLE*(1-exp(-bMLE*data)))}
 
 #log-likelihood function
-GO_BM_FT_LLF <- function(aMLE,bMLE,data){
- 		lnl <- -aMLE*(1-exp(-bMLE*tn))+n*log(aMLE)+n*log(bMLE)-bMLE*sum(data)
- 		return(lnl)
-}
+GO_BM_FT_LLF <- function(aMLE,bMLE,data){	return( -aMLE*(1-exp(-bMLE*tn))+n*log(aMLE)+n*log(bMLE)-bMLE*sum(data))}
 
 #Estimated number of faults remaining
 
-GO_BM_FT_FaultsRemaining <- function(aMLE,n){
-	FaultsRemaining <- abs(aMLE-n)
-	return(FaultsRemaining )
-}
+GO_BM_FT_FaultsRemaining <- function(aMLE,n){return( abs(aMLE-n))}
 
 #Reliability
-GO_BM_FT_Reliability <- function(bMLE,tn){
-	Reliability <- exp(-bMLE*tn)
-	return(Reliability)
-}
+GO_BM_FT_Reliability <- function(bMLE,tn){return( exp(-bMLE*tn))}
 
 #Mean-time-to-failure (MTTF)
-GO_BM_FT_MTTF <- function(bMLE){
-	MTTF <- 1/bMLE
-	return(MTTF)
-	}
+GO_BM_FT_MTTF <- function(bMLE){return( 1/bMLE)}
