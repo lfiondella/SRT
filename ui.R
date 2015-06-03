2a0f04ff7f1df03a680b9a47204644b5d125e148
Robert Davis <rdavis2@umassd.edu>
2015-06-02 20:48:40 -0400

    more output changes

--- ui.R
+++ ui.R
@@ -38,7 +38,6 @@ tags$head(includeScript("analytics/google-analytics.js")),
 
     # Show a plot of the generated distribution
     mainPanel(
-      #textOutput("text1"),
       plotOutput("distPlot", height = "700px")
    ,width=9)
   ),
